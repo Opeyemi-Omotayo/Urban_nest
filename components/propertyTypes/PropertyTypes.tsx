42afdf196e513f72fbfc17297c0bf43bb1744cc4
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-10-01 17:50:49 +0100

    clean up

--- components/propertyTypes/PropertyTypes.tsx
+++ components/propertyTypes/PropertyTypes.tsx
@@ -6,7 +6,7 @@ const PropertyTypes = () => {
     <>
       <section className="flex items-center justify-center flex-col mt-[5.5rem] mb-[4rem]">
         <h1 className="text-3xl font-bold mb-4">Property Types</h1>
-        <p className="text-gray-800 w-[70%] text-center">
+        <p className="text-gray-800 w-[90%] lg:w-[70%] text-center">
           Choose from a variety of residential options, including spacious
           apartments, luxurious penthouses, and elegant townhouses, all designed
           to cater to your unique lifestyle. We offer versatile commercial
