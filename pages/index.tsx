7724291de50ab7c9795b34cb6657f0f8f7d0b67c
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-10-01 13:37:36 +0100

    head tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import Header from '@/components/header/Header'
+import Header from '@/components/header/Header';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <main
       className="bg-red-500"
     >
+      <Head>
+        <title>Urban Nest</title>
+        <meta
+          name="description"
+          content="The Best Platform for real estate"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Header />
     </main>
   )
