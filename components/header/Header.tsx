2029fc440cdffd7327e165ee10cd84a3733c54c2
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-10-03 16:22:47 +0100

    Navigation

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { TfiClose } from "react-icons/tfi";
 import { AiOutlineAlignRight } from "react-icons/ai";
-import NavBar from "../navBar/NavBar";
+import Navigation from "../navigation/Navigation";
 
 const Header = () => {
     const [nav, setNav] = useState(false);
@@ -33,7 +33,7 @@ const Header = () => {
                     </h1>
 
                     <ul>
-                       <NavBar />
+                       <Navigation />
                     </ul>
                 </div>
 
@@ -45,7 +45,7 @@ const Header = () => {
                         className={`absolute top-[63px] z-20 items-center w-full left-0 py-10  h-full ${nav ? "block" : "hidden"
                             }`}
                     >
-                        <NavBar />
+                        <Navigation />
                     </ul>
                     <div
                         className="block text-3xl lg:hidden"
