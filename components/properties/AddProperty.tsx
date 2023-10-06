a35028597cb3682be04ba6bc08baaa707eceac30
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-06 15:30:40 +0200

    form: add property

--- components/properties/AddProperty.tsx
+++ components/properties/AddProperty.tsx
@@ -1,79 +1,146 @@
-import React from 'react';
-import Input from '../elements/input/Input';
-import { VALIDATOR_REQUIRE } from '@/validation/Validation';
+import React from "react";
+import Input from "../elements/input/Input";
+import { VALIDATOR_REQUIRE } from "@/validation/Validation";
+import { useForm } from "@/hooks/useForm";
 
 const AddProperty = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+      propertyType: {
+        value: "",
+        isValid: false,
+      },
+      propertyInfo: {
+        value: "",
+        isValid: false,
+      },
+      location: {
+        value: "",
+        isValid: false,
+      },
+      bed: {
+        value: "",
+        isValid: false,
+      },
+      bath: {
+        value: "",
+        isValid: false,
+      },
+      measurement: {
+        value: "",
+        isValid: false,
+      },
+      price: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
   return (
     <div>
       <form>
-        <div>
-            <label htmlFor='name'>Name</label>
-            <input type='text' id='name' />
-        </div> 
         <Input
-            id="name"
-            element="input"
-            type="text"
-            label="Name"
-            placeholder="Enter the description of the property"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter your correct email address."
-            onInput={}
-          />
-        <div>
-            <h1>Property Type</h1>
-        <select id="propertyType" name="propertyType" className='w-[80%] mb-2 lg:mb-0 lg:w-[33%] px-4 py-3 rounded-lg outline-none'>
-                <option value="apartment">Apartment</option>
-                <option value="building">Building</option>
-                <option value="office">Office</option>
-            </select>
-        </div> 
-        <div>
-        <h1>Property Info</h1>
+          id="name"
+          element="input"
+          type="text"
+          label="Name"
+          placeholder="Enter the description of the property"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the description of the property."
+          onInput={inputHandler}
+        />
+        <Input
+          id="propertyType"
+          element="select"
+          label="Property Type"
+          options={[
+            { value: "", displayValue: "Enter the type of property" },
+            { value: "apartment", displayValue: "Apartment" },
+            { value: "building", displayValue: "Building" },
+            { value: "office", displayValue: "Office" },
+          ]}
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the type of property here."
+          onInput={inputHandler}
+        />
+        <Input
+          id="propertyInfo"
+          element="select"
+          label="Property Info"
+          options={[
+            { value: "", displayValue: "Enter the info of property" },
+            { value: "rent", displayValue: "Rent" },
+            { value: "buy", displayValue: "Buy" },
+          ]}
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the info of property here."
+          onInput={inputHandler}
+        />
+        <Input
+          id="location"
+          element="select"
+          label="Location"
+          options={[
+            { value: "", displayValue: "Enter the location of the property" },
+            { value: "berlin", displayValue: "Berlin" },
+            { value: "munich", displayValue: "Munich" },
+            { value: "hamburg", displayValue: "Hamburg" },
+            { value: "cologne", displayValue: "Cologne" },
+          ]}
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the location of the property here."
+          onInput={inputHandler}
+        />
+        <Input
+          id="price"
+          element="input"
+          type="text"
+          label="Price"
+          placeholder="Enter the price of the property"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the price of the property here."
+          onInput={inputHandler}
+        />
+        <Input
+          id="bed"
+          element="input"
+          type="text"
+          label="Bed"
+          placeholder="Enter the number of bed in the property"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the number of bed in the property here."
+          onInput={inputHandler}
+        />
+        <Input
+          id="bath"
+          element="input"
+          type="text"
+          label="Bath"
+          placeholder="Enter the number of bath in the property"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the number of bath in the property here."
+          onInput={inputHandler}
+        />
 
-        <select id="propertyInfo" name="propertyInfo" className='w-[80%] mb-2 lg:mb-0 lg:w-[33%] px-4 py-3 rounded-lg outline-none'>
-                <option value="rent">rent</option>
-                <option value="buy">Buy</option>
-            </select>
-        </div> 
-        <div>
-        <h1>Location</h1>
-        <select id="location" name="location" className='w-[80%] mb-2 lg:mb-0 lg:w-[33%] px-4 py-3 rounded-lg outline-none'>
-                <option value="berlin">Berlin</option>
-                <option value="munich">Munich</option>
-                <option value="hamburg">Hamburg</option>
-                <option value="cologne">Cologne</option>
-            </select>
-        </div> 
-        <div>
-            <label htmlFor='price'>Price</label>
-            <input type='text' id='price' />
-        </div> 
-        <div>
-            <label htmlFor='bed'>Bed</label>
-            <input type='text' id='bed' />
-        </div> 
-        <div>
-            <label htmlFor='bath'>Bath</label>
-            <input type='text' id='bath' />
-        </div> 
-        <div>
-            <label htmlFor='measurement'>Measurement</label>
-            <input type='text' id='measurement' />
-        </div> 
         <Input
-            id="email"
-            element="input"
-            type="text"
-            label="Email"
-            placeholder="Enter your email address"
-            validators={[VALIDATOR_EMAIL()]}
-            errorText="Please enter your correct email address."
-            onInput={inputHandler}
-          />
+          id="measurement"
+          element="input"
+          type="text"
+          label="Measurement"
+          placeholder="Enter the room measurement of the property"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter the room measurement of the property here."
+          onInput={inputHandler}
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default AddProperty;
